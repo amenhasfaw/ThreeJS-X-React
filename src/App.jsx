0faccebe6015f9db9a82d70c90fed90e29ac1cc1
amenhasfaw <105349999+amenhasfaw@users.noreply.github.com>
2023-04-21 12:10:12 +0530

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter } from "react-router-dom"
-import {About, Contact, Experience, Feedbacks} from './components'
+import {About, Contact, Experience, Feedbacks, Hero} from './components'
 
 const App = () =>{
 
