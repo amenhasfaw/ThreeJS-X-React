3b1c55994e318642c0959fe78da38490ed1381f7
amenhasfaw <105349999+amenhasfaw@users.noreply.github.com>
2023-04-29 15:54:18 +0530

    Hero+Navbar Component

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -5,7 +5,7 @@ import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
 
 import CanvasLoader from '../Loader'
 
-const Computers = () => {
+const Computers = ({isMobile}) => {
   const computer = useGLTF('./desktop_pc/scene.gltf')
 
 
@@ -21,8 +21,8 @@ const Computers = () => {
                  shadow-mapSize={1024}/>
 
       <primitive object={computer.scene}
-                 scale={0.75}
-                 position={[0,-3.25,-1.5]}
+                 scale={isMobile ? 0.7 : 0.75}
+                 position={isMobile ? [0,-3,-2.2]:[0,-3.25,-1.5]}
                  rotation={[-0.01, -0.2, -0.1]}/>
 
     </mesh>
@@ -31,10 +31,29 @@ const Computers = () => {
 
 
 function ComputersCanvas() {
+  const [isMobile, setIsMobile] = useState("false")
+
+  useEffect(()=>{
+    const mediaQuery = window.matchMedia('(max-width: 500px)')
+
+    setIsMobile(mediaQuery.matches)
+
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    }
+
+    mediaQuery.addEventListener("change", handleMediaQueryChange)
+
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange)
+    }
+  }, [])
+
   return (
     <Canvas
       frameloop='demand'
       shadows
+      dpr={[1,2]}
       camera={{position: [20,3,5], fov:25}}
       gl={{preserveDrawingBuffer:true}}>
 
@@ -43,12 +62,13 @@ function ComputersCanvas() {
             enableZoom={false}
             maxPolarAngle={Math.PI / 2}
             minPolarAngle={Math.PI / 2}/>
-          <Computers/>
+          <Computers isMobile={isMobile}/>
         </Suspense>
+
         <Preload all/>
     </Canvas>
   )
 }
 
 
-export default ComputersCanvas
+export default ComputersCanvas;
