26f3ec1452a223e749f91d75fecd302748b48bdf
amenhasfaw <105349999+amenhasfaw@users.noreply.github.com>
2023-05-01 17:02:32 +0530

    TechStack Balls done

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -4,7 +4,13 @@ import { technologies } from "../constants"
 
 const Tech = () => {
   return (
-    <div>Tech</div>
+    <div className="flex flex-row gap-10 flex-wrap justify-center">
+      {technologies.map((tech) => (
+        <div className="w-28 h-28" key={tech.name}>
+          <BallCanvas icon={tech.icon} />
+        </div>
+      ))}
+    </div>
   )
 }
 
